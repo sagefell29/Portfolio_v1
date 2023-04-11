6e7d9893161d3bbce094a274caed7d2f517a244d
Rishi Kaushal <rishi29work@gmail.com>
2023-04-11 22:14:22 +0530

    More Resposiveness

--- src/components/NavBar/NavButton.js
+++ src/components/NavBar/NavButton.js
@@ -10,7 +10,7 @@ export const NavButton = props => {
         fontSize="lg"
         as={Link}
         href={`${props.link}`}
-        _hover={{ md:
+        _hover={{ lg:
           {transform: 'scale(1.4)',
           boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.75)',
           transition: 'all 0.1s ease-in-out'}
