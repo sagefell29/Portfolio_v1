2517eca86327f153049a30eefa0db26e40e66cb4
Rishi Kaushal <rishi29work@gmail.com>
2025-04-14 16:45:53 +0530

    Merge branch 'main' of https://github.com/sagefell29/Portfolio_v1

--- src/components/NavBar/NavButton.js
+++ src/components/NavBar/NavButton.js
@@ -2,7 +2,7 @@ import { Button, Tooltip } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 
 export const NavButton = ({ label, icon, path }) => {
-  const activeStyle = { color: 'teal' };
+  const activeStyle = { color: 'white' };
 
   return (
     <Tooltip label={label}>
@@ -11,14 +11,20 @@ export const NavButton = ({ label, icon, path }) => {
         to={path}
         exact="true"
         _activeLink={activeStyle}
-        color='#0a696c'
+        color="#0a696c"
         bg="#29232e"
         fontSize={['sm', 'md', 'lg']}
         p={['2', '4', '6']}
         w={['auto', '50px', '100px']}
-        h={['40px', '50px', '60px']} 
+        h={['40px', '50px', '60px']}
         justifyContent="center"
         alignItems="center"
+        _hover={{
+          bg: '#29232e',
+          color: '#00ffff',
+          boxShadow: '0 0 10px rgba(0, 255, 255, 0.2)',
+        }}
+        transition="all 0.2s ease-in-out"
       >
         {icon}
       </Button>
